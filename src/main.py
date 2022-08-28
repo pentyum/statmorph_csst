3df5a7500ad81cb68f70e30135db1da0939f0664
pentyum <pentyum@189.cn>
2022-08-28 09:04:08 +0800

    添加自动命名输出文件

--- src/main.py
+++ src/main.py
@@ -293,7 +293,7 @@ help_str = """SExtractor-Statmorph 简化合并版使用说明
 	-i, --image_file=原始图像文件(未扣除背景)，双图像模式中指用来探测源的图像文件，若跳过SExtractor可以不需要
 	-y, --measure_file=双图像模式中用于测量的图像文件(未扣除背景)，若不指定(为null)则为单图像模式，若跳过SExtractor可以不需要
 	-w, --wht_file=权重图像文件，若跳过SExtractor可以不需要
-	-o, --save_file=形态学参数输出文件名
+	-o, --save_file=形态学参数输出文件名，若不指定则默认为image_file的文件名(不包括后缀).txt，双图像模式则还包括measure_file
 	-p, --run_percentage=运行全部源数量的百分比，100表示全部运行
 	-l, --run_specified_label=仅运行指定编号的源，若为0则运行全部源
 	-s, --sextractor_work_dir=SExtractor的输出文件存放文件夹，若不指定则默认为image_file的文件名(不包括后缀)，双图像模式则还包括measure_file
@@ -381,11 +381,15 @@ def main(argv) -> int:
 		output_image_dir = None
 	if not check_not_null(image_compare_file):
 		image_compare_file = None
+
+	run_name = get_basename_without_end(detect_file)
+	if measure_file is not None:
+		run_name = run_name + "_" + get_basename_without_end(measure_file)
+
 	if not check_not_null(sextractor_work_dir):
-		detect_base_name = get_basename_without_end(detect_file)
-		sextractor_work_dir = "sextractor_"+detect_base_name
-		if measure_file is not None:
-			sextractor_work_dir = sextractor_work_dir + "_" + get_basename_without_end(measure_file)
+		sextractor_work_dir = "sextractor_" + run_name
+	if not check_not_null(save_file):
+		save_file = "statmorph_" + run_name + ".txt"
 
 	if threads <= 0:
 		threads = min(multiprocessing.cpu_count() - 1, 1)
