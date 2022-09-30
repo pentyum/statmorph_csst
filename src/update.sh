bd4a6106b58748394b2cd28a503263ed913ee9fb
pentyum <pentyum@189.cn>
2022-09-30 22:02:40 +0800

    优化更新脚本

--- src/update.sh
+++ src/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-mv config.properties config.properties.bak
+CONFIG_FILE=config.properties
+mv $CONFIG_FILE "${CONFIG_FILE}.bak"
 cd ../
 git pull
 ./build.sh
 cd ./src
+if test -f "$CONFIG_FILE"; then
+  echo "配置文件已更新"
+else
+  mv "${CONFIG_FILE}.bak" $CONFIG_FILE
+  echo "配置文件未更新"
+fi
