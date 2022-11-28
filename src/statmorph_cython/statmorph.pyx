7c689de56c75e6c8953567253581405116c58aca
pentyum <pentyum@189.cn>
2022-11-28 13:58:10 +0800

    修改图像输出形式

--- src/statmorph_cython/statmorph.pyx
+++ src/statmorph_cython/statmorph.pyx
@@ -633,8 +633,10 @@ cdef class BaseInfo(MorphInfo):
 		else:
 			plt.figure(figsize=(20, 20))
 
-		extent = (stamp_x, self._slice_stamp[1].stop, stamp_y, self._slice_stamp[0].stop)
-		plt.imshow(np.log10(self._cutout_stamp_maskzeroed), cmap="gray_r", origin="lower", extent=extent, interpolation="none")
+		cdef tuple extent = (stamp_x, self._slice_stamp[1].stop, stamp_y, self._slice_stamp[0].stop)
+		plt.imshow(self._cutout_stamp_maskzeroed, cmap="gray_r", origin="lower", extent=extent)
+		cdef double vmax = np.percentile(self._cutout_stamp_maskzeroed, 80)
+		plt.clim(0, vmax)
 
 		if self.cas is not None:
 			rec_x = stamp_x + self.cas._slice_skybox[1].start
